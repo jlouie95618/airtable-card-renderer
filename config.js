0844722af84ada18bcddc2ba6fdd74a1e7f22ffe
John Louie <jlouie95618@gmail.com>
2015-07-17 16:03:45 -0700

    made some changes to styling to accomodate for the word wrap of the email; probably not final

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var config = {
     stagingAppId: 'sdk_airtable-ch-sta_3816999c84',
     developmentAppId: 'sdk_airtable-ch-dev_d41135c420', 
     chromeExtension: 'chrome-extension://',
-    defaultStyling: '/css/default.css', // switch back to default once done testing '/css/sidebar_style.css',//
+    defaultStyling: '/css/sidebar_style.css',//'/css/default.css', // switch back to default once done testing
     compactStyling: '/css/compact.css',
     expandedStyling: '/css/expanded.css',
     mailToIcon: '/email_icon.png'
