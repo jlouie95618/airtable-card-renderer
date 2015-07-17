44bffd2181c086cce7c3d339072309015ad72ed8
John Louie <jlouie95618@gmail.com>
2015-07-17 12:00:21 -0700

    needed to change some styling and use class rather than id for mail-to-icon in the email_column_type; allso tried to include an if statement to accomodate a potentially non chrome extension situation

--- types/email_column_type.js
+++ types/email_column_type.js
@@ -30,10 +30,15 @@ var EmailColumnType = GenericColumnType.extend({
     },
     _createEmailIcon: function() {
         var mailToIcon = $('<img>');
-        mailToIcon.attr('id', 'mail-to-icon');
+        mailToIcon.attr('class', 'mail-to-icon');
         mailToIcon.attr('alt', 'mail to icon');
-        mailToIcon.attr('src', this._config.chromeExtension + 
-            chrome.runtime.id + this._config.mailToIcon);
+        if (chrome.runtime) {
+            mailToIcon.attr('src', this._config.chromeExtension + 
+                chrome.runtime.id + this._config.mailToIcon);
+        } else {
+            // Non-chrome extension version:
+            mailToIcon.attr('src', '.' + this._config.mailToIcon);
+        }
         return mailToIcon;
     }
 });
