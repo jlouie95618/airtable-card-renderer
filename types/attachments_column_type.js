0f478879dbac9934a9131467c7b115bda61b9db7
John Louie <jlouie95618@gmail.com>
2015-07-29 11:23:50 -0700

    added the js to achieve the grid image rendering

--- types/attachments_column_type.js
+++ types/attachments_column_type.js
@@ -22,27 +22,43 @@
         init: function(columnName, contentObject, verbose) {
             this._super(columnName, contentObject, verbose);
             this._numImages = this._determineNumImages(contentObject.displayValue);
-            this._singleImage = 'img-content';
-            this._gridOfImages = 'img-content-grid';
         },
         generateElement: function(isForCompact) {
+            var that = this;
             var content = $('<div/>');
+            var images;
+            var docs;
             _.each(this._displayValue, function(attachmentObject) {
                 var anchor = $('<a/>').attr('href', attachmentObject.url);
                 var icon;
                 var iconText;
+                var image;
                 if ((attachmentObject.type).indexOf('image') !== -1) {
-                    var image = $('<img/>').attr('src', attachmentObject.url);
-                    image.attr('alt', attachmentObject.filename + ' ');
-                    anchor.append(image.attr('class', 'img-content'));
+                    if (that._numImages > 1) {
+                        image = $('<div/>').attr('class', 'img-content-grid');
+                        image.css('background-image', 
+                            'url(' + attachmentObject.url + ')');
+                        anchor.append(image);
+                    } else {
+                        image = $('<img/>').attr('src', attachmentObject.url);
+                        image.attr('alt', attachmentObject.filename);
+                        anchor.append(image.attr('class', 'img-content'));
+                    }
+                    if (!images) { images = $('<div/>'); }
+                    images.append(anchor);
+
                 } else {
                     iconText = '  ' + attachmentObject.filename;
                     icon = $('<i/>').attr('class', 
                         'airtable-gmail-ext-icon-file-alt').text(iconText);
                     anchor.append(icon);
+                    if (!docs) { docs = $('<div/>'); }
+                    docs.append(anchor);
                 }
-                content.append($('<div/>').append(anchor));
             });
+
+            if (images) { content.append(images); }
+            if (docs) { content.append(docs); }
 
             return this._createBasicLayout(isForCompact, 
                     this._columnName, content); 
--- types/lookup_column_type.js
+++ types/lookup_column_type.js
@@ -34,29 +34,44 @@
             var list = '';
             var elemNum = 0;
             var dateArray;
+            var numImages;
+            var images;
+            var docs;
             if (this._verbose) {
                 console.log('lookupResultType: ', this._lookupResultType);
             }
             if (this._lookupResultType === 'multipleAttachment') {
+                numImages = this._determineNumImages(this._displayValue);
                 _.each(this._displayValue, function(item) {
                     var anchor;
                     var icon;
                     var iconText;
+                    var image;
                     // The case when the item is an object i.e. a document,
                     //  file or picture, etc.
                     if (typeof item === 'object') {
                         anchor = $('<a/>').attr('href', item.url);
                         if ((item.type).indexOf('image') !== -1) {
-                            var image = $('<img/>').attr('src', item.url);
-                            image.attr('alt', item.filename);
-                            anchor.append(image.attr('class', 'img-content'));
+                            if (numImages > 1) {
+                                image = $('<div/>').attr('class', 'img-content-grid');
+                                image.css('background-image', 'url(' + item.url + ')');
+                                anchor.append(image);
+                            } else {
+                                image = $('<img/>').attr('src', item.url);
+                                image.attr('alt', item.filename);
+                                anchor.append(image.attr('class', 'img-content'));
+                            }
+                            if (!images) { images = $('<div/>'); }
+                            images.append(anchor);
                         } else {
                             iconText = '  ' + item.filename;
                             icon = $('<i/>').attr('class', 
                                 'airtable-gmail-ext-icon-file-alt').text(iconText);
                             anchor.append(icon);
+                            if (!docs) { docs = $('<div/>'); }
+                            docs.append(anchor);
                         }
-                        content.append(anchor);
+
                     } else { // Case when the column contains string/number values
                         item = _.escape(item);
                         if (elemNum === (that._displayValue.length - 1)) {
@@ -82,9 +97,23 @@
                 content = this._displayValue;
             }
 
+            if (images) { content.append(images); }
+            if (docs) { content.append(docs); }
+
             return this._createBasicLayout(isForCompact, 
                     this._columnName, content);
-        }
+        },
+        _determineNumImages: function(displayValue) {
+            var numImages = 0;
+            _.each(displayValue, function(value) {
+                if (value && typeof value.type === 'string' && 
+                    (value.type).indexOf('image') === 0) {
+                    numImages++;
+                }
+            });
+            if (this._verbose) { console.log('numImages: ', numImages); }
+            return numImages;
+        }        
     });
 
     if (typeof exports !== 'undefined') {
